08fec4319d40884b4a6fd5d2bef9ec5005b6c656
Gurkaran Dhami <gurkaran.dhami@mail.utoronto.ca>
2019-05-14 09:42:16 -0400

    This is the first commit

---
+++ Account-Creation/code.js
@@ -0,0 +1,13 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+
+function validateName() {
+  const name = document.getElementById('name');
+  const re = /^[a-zA-Z]{2,10}$/;
+
+  if(!re.test(name.value)){
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
